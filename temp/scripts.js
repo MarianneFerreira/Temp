be4766072f02c3328eb372087e939cf7003f6145
Marianne de Andrade Ferreira <marianneaferreira@gmail.com>
2023-11-30 23:56:15 -0300

    Jesus

---
+++ temp/scripts.js
@@ -0,0 +1,13 @@
+const key = "3875011597233dc4414e5ac75f8f8054"
+
+
+
+// document é usando quando procurando um doc no html 
+// querySelector serve para procurar algo que estaá dentro do html
+// So de colocar . ele sabe que buscamos uma classe
+function button() {
+    const input = document.querySelector (".input-cidade").value
+
+    console.log(input)
+
+}
